5cc20303ad74354332225321d11141d072141aac
Piotr <piotr.dylnicki@gmail.com>
2024-03-07 22:48:00 +0200

    4

--- travel_app/models.py
+++ travel_app/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 
-# class Employee(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=255)
-#
-# class BusinessTrip(models.Model):
-#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-#     destination = models.CharField(max_length=255)
-#     start_date = models.DateField()
-#     end_date = models.DateField()
+COUNTRY = (
+    ("Poland", "Poland"),
+    ("Germany", "Germany"),
+    ("France", "France"),
+    ("Spain", "Spain"),
+    ("Italy", "Italy"),
+    ("Czech Republic", "Czech Republic"),
+    ("Slovakia", "Slovakia"),
+    ("Austria", "Austria"),
+    ("Hungary", "Hungary")
+)
+
+class Employee(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+class BusinessTrip(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    destination = models.CharField(max_length=255, choices=COUNTRY)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Expense(models.Model):
+    business_trip = models.ForeignKey(BusinessTrip, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
